51aee0016d726e62fc22cd012b424ea112ad74f1
lynnemunini <muninilynne65@gmail.com>
2022-11-03 12:01:01 +0300

    Initial commit

--- app/src/main/java/com/example/godutch/MainActivity.kt
+++ app/src/main/java/com/example/godutch/MainActivity.kt
@@ -18,26 +18,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             GoDutchTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     GoDutchTheme {
-        Greeting("Android")
+
     }
 }
